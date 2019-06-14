7b0461741d31263e2ed827365f7e55d3443cb140
rodgnathaniel <rodgnathaniel@gmail.com>
2019-06-13 17:24:15 -0700

    \

--- community_potluck/events/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-app_name = 'events'
-
-urlpatterns = [    
-    path('', views.events_view, name='events'),
-]
--- community_potluck/events/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-from django.http import HttpResponse, JsonResponse
-
-import json
-
-def events_view(request):
-
-    return render(request, "events/events_home.html", {})
